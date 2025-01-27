991ad0aae45118e2fb61b0e3773a2655ce7c9644
ThienAnUIT2022 <22520028@gm.uit.edu.vn>
2025-01-27 14:16:51 +0700

    update site title and tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "little Whale's Site",
+  tagline: "Dinosaurs and Whales are cool",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
